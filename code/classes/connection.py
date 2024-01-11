b4aa4b893b40e30cf998c0db606e2667d9d0b304
LiekeZ <liekez@DESKTOP-IVHCHHE>
2024-01-11 10:37:08 +0100

    better file structure in directory

---
+++ code/classes/connection.py
@@ -0,0 +1,6 @@
+class Connection():
+
+    def __init__(self, station_1: str, station_2: str, distance: float):
+        self.station_1 = station_1
+        self.station_2 = station_2
+        self.distance = distance
