e253cf55a12cc27562a1f6350f854f6dbbbba9e4
Gert Hakkenberg <gerthakkenberg@gmail.com>
2024-01-12 16:44:14 +0100

    removed debug print statements

--- code/classes/state.py
+++ code/classes/state.py
@@ -297,20 +297,13 @@ class State():
         returns:
             bool for overall constraint satisfaction     
         """
-        # DEBUGGING
-        print(f"Time frame valid? {self.routes_valid_time_frame()}")
-        print(f"Max routes reached? {self.less_than_max_routes()}")
-        print(f"All connections used? {self.all_connections_used()}")
 
         if not relaxed_time_frame and not self.routes_valid_time_frame():
-            print(
-                f"time frame constraint is not relaxed and a route exceeds the time frame.")
             return False
         if not relaxed_max_routes and not self.less_than_max_routes():
             return False
         if not relaxed_all_connections and not self.all_connections_used():
             return False
-        print("returning True")
         return True
 
     def show(self, relaxed_time_frame: bool = False, relaxed_max_routes: bool = False, relaxed_all_connections: bool = False):
