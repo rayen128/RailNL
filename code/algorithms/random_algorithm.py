0c0a464b563f331182edcb4a1b7f47c23f964661
rayen128 <rayenoaf@live.nl>
2024-01-12 15:33:12 +0100

    algorithm 2 edit

--- code/algorithms/random_algorithm.py
+++ code/algorithms/random_algorithm.py
@@ -41,7 +41,7 @@ def random_algorithm_2(state: 'State') -> tuple[float, 'Route', str]:
         state.add_route(random.choice(state.connections))
 
         # add routes until time_frame is exceeded
-        while route.is_valid_time():
+        while state.routes[current_route_index].is_valid_time():
 
             # choose random new route
             new_connection = random.choice(
@@ -60,4 +60,35 @@ def random_algorithm_2(state: 'State') -> tuple[float, 'Route', str]:
     return score, route, description
 
 
-# Algorithme 3 = constrained random
+def random_algorithm_3(state: 'State') -> tuple[float, 'Route', str]:
+    """
+    # TODO: doc-string    
+    """
+
+    # variable to keep track of current route
+    current_route_index = 0
+
+    # do loop until there is a valid solution (while max routes can be exceeded)
+    while not state.is_valid_solution(relaxed_max_routes=True):
+
+        # pick random connection and create route
+        state.add_route(random.choice(state.connections))
+
+        # add routes until time_frame is exceeded
+        while route.is_valid_time():
+
+            # choose random new route
+            new_connection = random.choice(
+                state.routes[current_route_index].get_end_station().get_connections())
+
+            state.routes[current_route_index].add_connection(new_connection)
+
+        state.routes[current_route_index].delete_connection_end()
+        current_route_index += 1
+
+    # save return variables
+    score = state.calculate_score()
+    route = state.routes[0]
+    description = state.show()
+
+    return score, route, description
