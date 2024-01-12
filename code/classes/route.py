478a914ec3636fd325b9449235c4cddeeeaa01d7
LiekeZ <liekez@DESKTOP-IVHCHHE>
2024-01-12 11:01:59 +0100

    updated route.py with docstrings and assertions

--- code/classes/route.py
+++ code/classes/route.py
@@ -3,7 +3,7 @@ from connection import Connection
 
 
 class Route():
-    def __init__(self: 'Route') -> None:
+    def __init__(self: 'Route', name: str) -> None:
         """
         initializes a Route class which maintains lists with information about a route
 
@@ -11,6 +11,7 @@ class Route():
             makes empty lists for the stations and the connections in a route
             sets the total time of a route at 0
         """
+        self.name: str = name
         self.route_stations: list = []
         self.route_connections: list = []
         self.total_time: float = 0
@@ -69,7 +70,7 @@ class Route():
             self.add_station_end(
                 self.get_other_station(connection, end_station))
             self.total_time += connection.distance
-
+            
         # check if start station has the connection, if true add connection
         elif start_station._has_connection(connection):
             self.route_connections.insert(0, connection)
@@ -78,13 +79,44 @@ class Route():
             self.total_time += connection.distance
 
     def add_station_end(self: 'Route', station: 'Station') -> None:
+        """
+        adds station at the end of the list of stations
+
+        pre: 
+            station is a Station class
+            self.route_stations is a list
+
+        post: 
+            the station object is added to the list of stations
+        """
         self.route_stations.append(station)
 
     def add_station_start(self: 'Route', station: 'Station') -> None:
+        """
+        adds station object to beginning of station list
+
+        pre: 
+            station is a Station class
+            self.route_stations is a list
+
+        post: 
+            the station object is added to the list of stations
+        """
         self.route_connections.insert(0, station)
 
     def get_other_station(self: 'Route', connection: 'Connection', station: 'Station') -> 'Station':
+        """
+        returns the other station in a connection than the given station
 
+        pre:
+            connection is a Connection class which maintains two stations
+            station is a Station class which maintains the name of the station
+
+        returns:
+            the other station in the connection than the given station if the given station is in connection
+            none if the given station is not in connection
+        """
+       
         # TODO: deze code veranderen wanneer de stationsobjecten opgeslagen liggen in de connection class
         if station.name == connection.station_1:
             return connection.station_2
@@ -94,26 +126,44 @@ class Route():
         
         return None
 
-    def delete_connection_end(self: 'Route', connection: 'Connection') -> None:
+    def delete_connection_end(self: 'Route') -> None:
         """
+        deletes the last connection
+        
         pre: 
-            given connection exists at begin or end of connections list
+            the stations and connections lists are not empty
+            the last station in stations list has the last connection in de connections list
 
         post: 
-            deletes given connection
+            deletes last connection from self.route_connections
+            deletes last station from slef.route_stations
+            substracts deleted connections distance from self.total_time
         """
+        assert self.route_connections != [] and self.route_stations != [] and \
+        self.total_time > 0 and self.route_stations[-1]._has_connection(self.route_connections[-1]),\
+        "delete last connection is not possible"
+
         connection = self.route_connections.pop()
-        self.route_connections.pop()
+        self.route_stations.pop()
         self.total_time -= connection.distance
 
     def delete_connection_start(self: 'Route', connection: 'Connection') -> None:
         """
+        deletes the first connection
+        
         pre: 
-            given connection exists at begin or end of connections list
+            the stations and connections lists are not empty
+            the first station in stations list has the first connection in de connections list
 
         post: 
-            deletes given connection
+            deletes first connection from self.route_connections
+            deletes first station from slef.route_stations
+            substracts deleted connections distance from self.total_time
         """
+        assert self.route_connections != [] and self.route_stations != [] and \
+        self.total_time > 0 and self.route_stations[0]._has_connection(self.route_connections[0]),\
+        "delete first connection is not possible"
+
         connection = self.route_connections.pop(0)
         self.route_connections.pop(0)
         self.total_time -= connection.distance
