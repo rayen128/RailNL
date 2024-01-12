25a4468f3dad278adc37e8eaec5386147e035de4
rayen128 <rayenoaf@live.nl>
2024-01-12 15:17:47 +0100

    algorithm 2 written

--- code/algorithms/random_algorithm.py
+++ code/algorithms/random_algorithm.py
@@ -4,16 +4,13 @@ import random
 
 def random_algorithm_1(state: 'State') -> tuple[float, 'Route', str]:
     """
-    pick random starting location/connection 
-    then pick random follow-up 
-    go on till all connections are picked
-    save score
+    # TODO: doc-string
     """
 
     # pick random connection and create route
     state.add_route(random.choice(state.connections))
 
-    # do loop until there is a valid solution
+    # do loop until there is a valid solution (while max time_frame per route can be exceeded)
     while not state.is_valid_solution(relaxed_time_frame=True):
         # choose random new route
         new_connection = random.choice(
@@ -27,3 +24,40 @@ def random_algorithm_1(state: 'State') -> tuple[float, 'Route', str]:
     description = state.show()
 
     return score, route, description
+
+
+def random_algorithm_2(state: 'State') -> tuple[float, 'Route', str]:
+    """
+    # TODO: doc-string    
+    """
+
+    # variable to keep track of current route
+    current_route_index = 0
+
+    # do loop until there is a valid solution (while max routes can be exceeded)
+    while not state.is_valid_solution(relaxed_max_routes=True):
+
+        # pick random connection and create route
+        state.add_route(random.choice(state.connections))
+
+        # add routes until time_frame is exceeded
+        while route.is_valid_time():
+
+            # choose random new route
+            new_connection = random.choice(
+                state.routes[current_route_index].get_end_station().get_connections())
+
+            state.routes[current_route_index].add_connection(new_connection)
+
+        state.routes[current_route_index].delete_connection_end()
+        current_route_index += 1
+
+    # save return variables
+    score = state.calculate_score()
+    route = state.routes[0]
+    description = state.show()
+
+    return score, route, description
+
+
+# Algorithme 3 = constrained random
