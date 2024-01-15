a7d942f0c52e520549d46294af2f54eafbd05cad
rayen128 <rayenoaf@live.nl>
2024-01-15 14:06:41 +0100

    small ideas

--- code/algorithms/algorithms_ideas.py
+++ code/algorithms/algorithms_ideas.py
@@ -1,2 +1,4 @@
+# kijk continu naar hoe de mogelijkheden je score beïnvloeden
 # algoritme waar per route wordt gecontroleerd of een verbinding niet al gebruikt wordt in een route
 # algoritme die door gaat tot het timeframe en dan een nieuw traject begint
+# identificeer of iets wel/niet een eind-station is (geef bijv. punten extra in je doelfunctie als je hier een route begint?)
