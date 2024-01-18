af17f17b165309b6a3c550dbe2c9cd9457307146
rayen128 <rayenoaf@live.nl>
2024-01-18 11:04:00 +0100

    algorithm ideas

--- code/algorithms/algorithms_ideas.py
+++ code/algorithms/algorithms_ideas.py
@@ -5,3 +5,9 @@
 
 # methode schrijven die bekijkt hoeveel het toevoegen van een connectie de punten veranderd?
 # methode schrijven die bekijkt hoeveel het verwijderen van een route de punten veranderd?
+
+# als je een connectie 2x moet gebruiken, probeer dan de korste route uit te kiezen
+# update de 'eind-stations' dynamisch op basis van de routes die gemaakt zijn (maak het starten hierin interessanter)
+# stations met oneven aantal routes aan zich verbonden zijn 'focus-punten'
+# kijk of routes 'ingesloten' (= als geen enkele ongebruikte route hieraan is verbonden)
+# kijken hoeveel van de totale time_frame een route gebruikt
