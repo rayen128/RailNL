2882437dd3e348acd71de9a7726c22a0c13de9d7
rayen128 <rayenoaf@live.nl>
2024-01-18 16:31:37 +0100

    started doc-strings for (Random_)Algorithm class

--- code/algorithms/random_algorithm.py
+++ code/algorithms/random_algorithm.py
@@ -8,19 +8,32 @@ class Algorithm():
     def __init__(self, state: 'State') -> None:
         self.state = state
 
-    def create_random_state(self, number_of_connections: int = 1):
+    def add_random_route(self) -> None:
         """
-        # TODO: doc-string
+        adds a random 1-length route to the state
+
+        pre: 
+            self.state is a state object
+            self.state.connections contains connection objects
+
+        post:
+            added a 1-length route to self.state.routes
+        """
+        self.state.add_route(random.choice(self.state.connections))
+
+    def create_random_state(self, number_of_connections: int = 1) -> None:
+        """
+        randomly generates a state with random 1-length routes (default=1)
+
+        pre: 
+            number_of_connections is an integer
+
+        post:
+            self.state.routes contains specified number of 1-length routes
         """
         for new_connection in range(number_of_connections):
             # pick random connection and create route
             self.state.add_random_route()
-
-    def add_random_route(self) -> None:
-        """
-        # TODO: doc-string
-        """
-        self.state.add_route(random.choice(self.state.connections))
 
     def add_random_connection(self, route_index: int = 0, choice: Union[str, None] = None) -> str:
         """
