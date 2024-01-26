02254bc1e642e62a0f49e90357549ced52ea1410
Gert Hakkenberg <gerthakkenberg@gmail.com>
2024-01-26 15:06:32 +0100

    further implemented scripts

--- code/scripts/experiment_hill_climber_iterations.py
+++ code/scripts/experiment_hill_climber_iterations.py
@@ -8,7 +8,12 @@ path.append("code/classes")
 from state import State
 
 
-def experiment_hill_climber_iterations(case_name: str, state: 'State'):
+def run_single_combination(iteration_start: int, iteration_end: int, state: 'State', hill_climber: 'Hill_climber'):
+    for i in range(iteration_start, iteration_end):
+        return hill_climber.run()
+
+
+def experiment_hill_climber_iterations(case_name: str, state: 'State', time_seconds: int):
     with open(f"data/experiment_hill_climber_iterations_{case_name}.csv", "w") as file:
         writer = csv.writer(file)
         writer.writerow(["id",
@@ -16,8 +21,16 @@ def experiment_hill_climber_iterations(case_name: str, state: 'State'):
                          "mutation",
                          "score_list"])
 
+        counter = 0
+
         # experiment with valid start state and light mutations
         hc = Hill_climber(state)
-        for i in range(1000):
-            score_list = hc.run()
+        start = time.time()
+        while time.time() - start < time_seconds / 4:
+            csv_list = hc.run()
             writer.writerow([i, "valid", "light", score_list])
+
+        hc = Hill_climber(state, valid_start_state=False)
+        for i in range(1000, 2000):
+            score_list = hc.run()
+            writer.writerow([i, "random", "light", score_list])
