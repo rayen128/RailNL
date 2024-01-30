94a3fdbf31fee4157cd423b9cb7938b180b6d4eb
rayen128 <rayenoaf@live.nl>
2024-01-30 18:00:05 +0100

    script.py

--- script.py
+++ script.py
@@ -3,4 +3,12 @@ from code.classes.state import State
 
 
 state = State('data/stations_holland.csv', 'data/routes_holland.csv', 7, 120)
-grid_search_PPA(state, 900, 'holland', 'hill_climber', 'sequential')
+
+# grid_search_PPA(state, 900, 'holland', 'hill_climber', 'sequential')
+
+grid_search_PPA(state, 600, 'holland', 'random', 'sequential')
+
+
+state('data/station_netherlands.csv', 'data/routes_netherlands.csv', 20, 180)
+grid_search_PPA(state, 900, 'netherlands', 'random', 'sequential')
+grid_search_PPA(state, 900, 'netherlands', 'hill_climber', 'sequential')
